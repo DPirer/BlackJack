88b484965894749aa646d425323d5a3cdc645458
RicardoCamachoDC <62330956+RicardoCamachoDC@users.noreply.github.com>
2020-04-15 12:21:45 +0200

    Update main.cpp

--- Blackjack1/main.cpp
+++ Blackjack1/main.cpp
@@ -21,10 +21,10 @@ int main()
 
 //funcion para crear una baraja el mazo de cartas, en funcion de la cantidad de barahas con los que se juegue.
 void crearBaraja(int n){
-    char numeros[]="A23456789JQK";
+    char numeros[]="A234567890JQK";
     char palo[]="DTPC";
 
-    baraja=(char**)malloc((n*56)*sizeof(char*));
+    baraja=(char**)malloc((n*52)*sizeof(char*));
     for (int i=1; i<=n; i++){
         for (int j=0; j<4; j++){
             for (int k=0; k<12; k++){
@@ -40,8 +40,8 @@ void crearBaraja(int n){
 void barajar(){
     int nrand;
     for (int i=0; i<2; i++){
-        for (int j=0; j<n*56; j++){
-            nrand=arc4random_uniform(n*56);
+        for (int j=0; j<n*52; j++){
+            nrand=arc4random_uniform(n*52);
             intercambiar(*&baraja[j][1], *&baraja[nrand][1]);
             intercambiar(*&baraja[j][2], *&baraja[nrand][2]);
         }
