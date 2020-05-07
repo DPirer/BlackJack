ca7f7c38a35583427ca31dac334bed1536025282
Alberto-Fogeda <62695918+Alberto-Fogeda@users.noreply.github.com>
2020-05-07 14:04:23 +0200

    BlackJack cuatro jugadores
    Ya se han incluido todas las mejoras mencionadas en la anterior versión. Ademas se ha modificado la semilla del generador del números aleatorios, consiguiendo así aumentar la fluidez del juego.
    Solo faltaría separar en un archivo de funciones, para tener mejor ordenado el código.

---
+++ BlackJack6/unjugador.h
@@ -0,0 +1,63 @@
+#ifndef UNJUGADOR_H
+#define UNJUGADOR_H
+
+#include <QWidget>
+#include "mano.h"
+#include <vector>
+
+namespace Ui {
+class UnJugador;
+}
+
+class UnJugador : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit UnJugador(QWidget *parent = nullptr);
+    ~UnJugador();
+
+private slots:
+    Mano repartirCarta();
+
+    void crearMano(int);
+
+    void resetear();
+
+    bool comprobarBlackjack(Mano [11]);
+
+    QString CrearDireccion(int,QString);
+
+    int ValorCarta(int,int,Mano [11],int *);
+
+    void crearBanca();
+
+    void mostrarCarta(int,QString);
+
+    void resultado(int);
+
+    void on_boton_empezar_clicked();
+
+    void on_boton_pedir_carta_clicked();
+
+    void on_boton_retirarse_clicked();
+
+    void on_boton_continuar_clicked();
+
+    void on_boton_reiniciar_clicked();
+
+    void on_boton_salir_clicked();
+
+    void on_boton_doblar_clicked();
+
+    void on_boton_dividir_clicked();
+
+    void on_boton_pedir_carta2_clicked();
+
+    void on_boton_doblar2_clicked();
+
+private:
+    Ui::UnJugador *ui;
+};
+
+#endif // UNJUGADOR_H
